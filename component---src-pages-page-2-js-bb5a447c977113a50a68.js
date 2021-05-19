f3dd40baf76af9b8795fbfdf79ad165d77f65ee3
adiala <david.adiala@gmail.com>
2021-05-19 20:28:14 -0300

    Updates

---
+++ component---src-pages-page-2-js-bb5a447c977113a50a68.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{9863:function(e,t,a){"use strict";a.r(t);var l=a(7294),n=a(5444),r=a(3651),c=a(3751);t.default=function(){return l.createElement(r.Z,null,l.createElement(c.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.rU,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-bb5a447c977113a50a68.js.map
